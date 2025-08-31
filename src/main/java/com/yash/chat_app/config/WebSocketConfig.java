6a89b7d15ef112f04430de7d80bd71126da24992
EdithTech <yash9atalkar@gmail.com>
2025-08-31 11:22:28 +0530

    config

--- src/main/java/com/yash/chat_app/config/WebSocketConfig.java
+++ src/main/java/com/yash/chat_app/config/WebSocketConfig.java
@@ -12,11 +12,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOrigins("http://localhost:3000").withSockJS();
+        registry.addEndpoint("/chat")
+                .setAllowedOriginPatterns("http://localhost:5173") // or "*"
+                .withSockJS();
     }
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config){
+    public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/app");
     }
